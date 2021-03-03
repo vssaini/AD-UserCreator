c52c5a30211bc0e05d257de707b81f3494904f66
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:11:08 +0530

    Add project files.

---
+++ UserCreator/FrmMain.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace UserCreator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblDomain = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtGivenName = new System.Windows.Forms.TextBox();
+            this.lblGivenName = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAdminUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdminPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblMessage.Location = new System.Drawing.Point(12, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(191, 15);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Create new user in Active Directory";
+            // 
+            // lblDomain
+            // 
+            this.lblDomain.AutoSize = true;
+            this.lblDomain.Location = new System.Drawing.Point(31, 49);
+            this.lblDomain.Name = "lblDomain";
+            this.lblDomain.Size = new System.Drawing.Size(52, 15);
+            this.lblDomain.TabIndex = 1;
+            this.lblDomain.Text = "Domain:";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(86, 41);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(164, 23);
+            this.txtDomain.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(86, 78);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(164, 23);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(20, 86);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(63, 15);
+            this.lblUsername.TabIndex = 5;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(86, 120);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(164, 23);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(23, 128);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(60, 15);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtGivenName
+            // 
+            this.txtGivenName.Location = new System.Drawing.Point(86, 163);
+            this.txtGivenName.Name = "txtGivenName";
+            this.txtGivenName.Size = new System.Drawing.Size(164, 23);
+            this.txtGivenName.TabIndex = 4;
+            // 
+            // lblGivenName
+            // 
+            this.lblGivenName.AutoSize = true;
+            this.lblGivenName.Location = new System.Drawing.Point(12, 171);
+            this.lblGivenName.Name = "lblGivenName";
+            this.lblGivenName.Size = new System.Drawing.Size(71, 15);
+            this.lblGivenName.TabIndex = 9;
+            this.lblGivenName.Text = "givenName:";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(86, 209);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(164, 23);
+            this.txtSurname.TabIndex = 5;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(23, 217);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(57, 15);
+            this.lblSurname.TabIndex = 11;
+            this.lblSurname.Text = "Surname:";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.Location = new System.Drawing.Point(194, 379);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateUser.TabIndex = 8;
+            this.btnCreateUser.Text = "Create";
+            this.btnCreateUser.UseVisualStyleBackColor = true;
+            this.btnCreateUser.Click += new System.EventHandler(this.btnCreateUser_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAdminUser);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAdminPassword);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 249);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 124);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Admin Credentials";
+            // 
+            // txtAdminUser
+            // 
+            this.txtAdminUser.Location = new System.Drawing.Point(71, 34);
+            this.txtAdminUser.Name = "txtAdminUser";
+            this.txtAdminUser.Size = new System.Drawing.Size(158, 23);
+            this.txtAdminUser.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Admin:";
+            // 
+            // txtAdminPassword
+            // 
+            this.txtAdminPassword.Location = new System.Drawing.Point(71, 83);
+            this.txtAdminPassword.Name = "txtAdminPassword";
+            this.txtAdminPassword.Size = new System.Drawing.Size(158, 23);
+            this.txtAdminPassword.TabIndex = 7;
+            this.txtAdminPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Password:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCreateUser);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.txtGivenName);
+            this.Controls.Add(this.lblGivenName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.lblDomain);
+            this.Controls.Add(this.lblMessage);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblDomain;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtGivenName;
+        private System.Windows.Forms.Label lblGivenName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button btnCreateUser;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtAdminUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdminPassword;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UserCreator/FrmMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace UserCreator
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            var currDomain = Domain.GetCurrentDomain();
+            txtDomain.Text = currDomain.Name;
+
+            txtUsername.Focus();
+        }
+
+        private void btnCreateUser_Click(object sender, EventArgs e)
+        {
+            var valid = false;
+            foreach (var txt in Controls.OfType<TextBox>().Select(control => control))
+            {
+                valid = CheckForEmpty(txt);
+            }
+
+            try
+            {
+                if (!valid) return;
+                CreateNewUser();
+                var msg = string.Format("User '{0}' created successfully!", txtUsername.Text);
+                MessageBox.Show(msg, "Congrats", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error occured \n\n" + exc, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        #region HELPERS
+
+        /// <summary>
+        /// Show error for text controls if the text is blank.
+        /// </summary>
+        /// <param name="textBox">The textbox control</param>
+        /// <returns>Return true if there is text else false</returns>
+        private bool CheckForEmpty(Control textBox)
+        {
+            bool valid;
+
+            if (textBox.Text.Trim().Length > 0)
+            {
+                valid = true;
+                errorProvider.SetError(textBox, string.Empty);
+            }
+            else
+            {
+                valid = false;
+                errorProvider.SetIconPadding(textBox, 5);
+                errorProvider.SetError(textBox, "Value can't be blank.");
+            }
+
+            return valid;
+        }
+
+        /// <summary>
+        /// Creates a new user on Active Directory
+        /// </summary>
+        public void CreateNewUser()
+        {
+            var principalContext = GetPrincipalContext(txtDomain.Text, txtAdminUser.Text, txtAdminPassword.Text);
+
+            var userPrincipal = new UserPrincipalEx(principalContext, txtUsername.Text, txtPassword.Text,true);
+
+            //User details
+            userPrincipal.UserPrincipalName = txtUsername.Text;
+            userPrincipal.GivenName = txtGivenName.Text;
+            userPrincipal.Surname = txtSurname.Text;
+            //userPrincipal.PasswordNeverExpires = true;
+            //userPrincipal.AllowReversiblePasswordEncryption = true;
+            userPrincipal.Save();
+
+            userPrincipal.SetNormalAccount = 512;
+            userPrincipal.Save();
+
+        }
+
+        /// <summary>
+        /// Gets the base principal context
+        /// </summary>
+        /// <returns>Returns the PrincipalContext object</returns>
+        public PrincipalContext GetPrincipalContext(string sDomain, string sServiceUser, string sServicePassword)
+        {
+            var oPrincipalContext = new PrincipalContext(ContextType.Domain, sDomain, sServiceUser, sServicePassword);
+            return oPrincipalContext;
+        }
+
+        /// <summary>
+        /// Gets a certain user on Active Directory
+        /// </summary>
+        /// <param name="sUserName">The username to get</param>
+        /// <returns>Returns the UserPrincipal Object</returns>
+        public UserPrincipal GetUser(string sUserName)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext(txtDomain.Text, txtAdminUser.Text, txtAdminPassword.Text);
+
+            UserPrincipal oUserPrincipal =
+               UserPrincipal.FindByIdentity(oPrincipalContext, sUserName);
+            return oUserPrincipal;
+        }
+
+
+        #endregion
+    }
+}
